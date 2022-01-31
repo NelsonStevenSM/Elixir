50d0ceb145f8f0fbff5aff9ad3f476af53991b48
NelsonStevenSM <nsanabiom@uni.pe>
2022-01-31 12:23:41 -0500

    Aplicaciones basicas

---
+++ cards/lib/cards.ex
@@ -0,0 +1,62 @@
+defmodule Cards do
+  @moduledoc """
+    Proporciona métodos para crear y obtener una baraja de cartas
+  """
+
+  @doc """
+    Retorna una lista de String representando una baraja de cartas
+  """
+  def create_deck do
+    values = ["1", "2", "3", "4", "5"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+        "#{value} of #{suit}"
+    end
+
+  end
+
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+    Divide una baraja en una mano y selecciona de la baraja. El argumento `hand_size` indica cuántas cartas debería tener en la mano
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["1 of Spades"]
+
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "No existe el archivo"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
+end
